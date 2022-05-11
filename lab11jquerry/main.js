f5ceafe2d1c36b21e7496ddcecd509e09091e73c
ANASS ABOUNOUH <anass.abounouh@gmail.com>
2022-05-11 01:29:23 -0500

    jquerry lab

---
+++ lab11jquerry/main.js
@@ -0,0 +1,48 @@
+
+
+$(document).ready(function() {
+
+
+    
+    let timer;
+    function start() {
+        timer = setInterval(grow, parseInt($("#interval").val()));
+    }
+
+    function grow() {
+        let size = parseInt($(".circlee").height())+ parseInt($("#growth").val());
+        $(".circlee").height(size);
+        $(".circlee").width(size);
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function draw() {
+        
+        let size = parseInt($("#width").val());
+        
+        let $a = $("<div class='circlee' id='cir'></div>");
+        
+        $("body").append($a);
+        $("#cir").css("background-color","yellow");
+        $("#cir").css("width",size);
+        $("#cir").css("height",size);
+        $("#cir").css("top", getRandomInt(size));
+        $("#cir").css("left", getRandomInt(size));
+        
+        $("#cir").click(function () {
+            this.remove();
+        });
+        
+    }
+
+    $( "#for" ).submit(function( event ) {
+        event.preventDefault();
+        draw();
+        start();
+      });
+    
+
+})
